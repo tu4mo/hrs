c6c8ff5c6d8ee9685b8d34b412b6ea6920a8f554
tu4mo <tu4mo@users.noreply.github.com>
2018-01-27 14:43:44 +0200

    Changing start defaults to current time

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -4,11 +4,13 @@ import { getOverview } from '../lib/overview'
 import Day from '../models/Day'
 
 export const start = (day: Day, args: string[]) => {
-  const [hours, minutes] = args[0].split(':')
-  day.startTime = setMinutes(
-    setHours(day.startTime, parseInt(hours, 10)),
-    parseInt(minutes, 10)
-  )
+  const now = new Date()
+
+  const [hours = now.getHours(), minutes = now.getMinutes()] = args[0]
+    ? args[0].split(':').map(_ => parseInt(_, 10))
+    : []
+
+  day.startTime = setMinutes(setHours(day.startTime, hours), minutes)
   day.save()
 
   console.log(getOverview(day))
--- src/lib/help.ts
+++ src/lib/help.ts
@@ -2,8 +2,8 @@ import { brighten, dim } from '../helpers/colors'
 
 // prettier-ignore
 export const getHelp = () => [
-  `Usage: ${brighten('hrs')}               ${dim('Show overview')}`,
-  `       ${brighten('hrs break')}         ${dim('Start a break')}`,
-  `       ${brighten('hrs note')} <note>   ${dim('Add note to current time')}`,
-  `       ${brighten('hrs start')} <hh:mm> ${dim('Change start time of the day')}`
+  `Usage: ${brighten('hrs')}                 ${dim('Show overview')}`,
+  `       ${brighten('hrs break')}           ${dim('Start a break')}`,
+  `       ${brighten('hrs note')} <note>     ${dim('Add note to current time')}`,
+  `       ${brighten('hrs start')} [<hh:mm>] ${dim('Change start time of the day (default: now)')}`
 ].join('\n')
